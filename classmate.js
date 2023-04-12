fd7269c661e8cfd3b395e8adc03a8befb6674ad6
tomato82 <117065828+tomato82@users.noreply.github.com>
2023-04-12 11:39:10 +0900

    Update classmate.js

--- classmate.js
+++ classmate.js
@@ -1,16 +1,31 @@
 const drum = new Audio('audio/drum.mp3');
 const symbal = new Audio('audio/symbal.mp3');
-function generateRandomNumber() {
-	return Math.floor(Math.random() * max.value) + 1; // 1����36�܂ł̃����_���Ȑ���Ԃ�
+let count = 0;
+let intervalList = [];
+let id = null;
+function generateRandomNumber(maxValue) {
+	return Math.floor(Math.random() * maxValue) + 1;
 }
+function keyPress(e){
+	if (e.key === " "){
+		onClick(e)
+	}
+}
+function onClick(e) {
+	count = iter.value;
+	generate(max.value);
 
-
-function generate() {
+}
+function generate(maxValue) {
 	var result = document.getElementById("result");
 	var frame = 0;
-	const id = setInterval(function(){
+	if (id !== null){
+		clearInterval(id)
+	}
+	id = setInterval(function(){
 		frame += 1;
-		result.innerHTML = generateRandomNumber();
+		resultNumber = String(generateRandomNumber(maxValue));
+		result.innerHTML = resultNumber;
 		if (Math.random()<1/10 && frame>10){
 			end();
 			clearInterval(id);
@@ -20,9 +35,18 @@ function generate() {
 	symbal.loop = true;
 }
 function end(){
+	var history = document.getElementById("history")
+	history.innerHTML += resultNumber+"<br/>";
+	count --
 	symbal.pause();
-	drum.play();
+	if (count===0){
+		drum.play();
+	}else{
+		setTimeout(()=>generate(max.value),1000);
+	}
 }
-var generateBtn = document.getElementById("generate-btn"); // �{�^�����擾
+var generateBtn = document.getElementById("generate-btn");
 var max = document.getElementById("range");
-generateBtn.addEventListener("click", generate); // �{�^���ɃN���b�N�C�x���g��ǉ�
+var iter = document.getElementById("iter");
+generateBtn.addEventListener("click", onClick);
+document.addEventListener("keypress",keyPress);
